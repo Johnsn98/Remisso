44426f7bf8f01d906dc86d21ddb13d89b79f5b83
Nathan Johnson <nathanjohnson@Nathans-MBP.lan>
2020-02-17 11:46:14 -0600

    functionsUser

---
+++ remisso-functions/dbSchema.js
@@ -0,0 +1,14 @@
+let db = {
+	posts: [
+		(name: 'Thomas'),
+		(userHandle: 'Nathan'),
+		(bodyAccount: 'Account of what happened'),
+		(bodyResolution: 'the user would like this to happen'),
+		(createdAt: 'date'),
+		(facebookLink: 'req.body.facebookLink'),
+		(instagramLink: 'req.body.instagramLink'),
+		(otherLink: 'req.body.otherLink'),
+		(likeCount: 4),
+		(commentCount: 2)
+	]
+};
--- remisso-functions/functions/index.js
+++ remisso-functions/functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
+// Service Account key
 var serviceAccount = require('./Account/serviceAccountKey.json');
 
 admin.initializeApp({
@@ -8,20 +10,38 @@ admin.initializeApp({
 	databaseURL: 'https://remisso-website.firebaseio.com/'
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send('Hello, this is remisso');
-});
+//Firebase configuration App
+var firebaseConfig = {
+	apiKey: 'AIzaSyAIGmi3SjqgFeEHkNB9J5SQx6kCok15j5I',
+	authDomain: 'remisso-website.firebaseapp.com',
+	databaseURL: 'https://remisso-website.firebaseio.com',
+	projectId: 'remisso-website',
+	storageBucket: 'remisso-website.appspot.com',
+	messagingSenderId: '165797964467',
+	appId: '1:165797964467:web:16c438d02e5367e5b29ec6',
+	measurementId: 'G-C4TT7V9R9Y'
+};
+
+const firebase = require('firebase');
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 // Get all posts function
-exports.getPosts = functions.https.onRequest((req, res) => {
-	admin
-		.firestore()
-		.collection('Posts')
+app.get('/posts', (req, res) => {
+	db.collection('Posts')
+		.orderBy('createdAt', 'desc')
 		.get()
 		.then((data) => {
 			let posts = [];
 			data.forEach((doc) => {
-				posts.push(doc.data());
+				posts.push({
+					postId: doc.id,
+					body: doc.data().body,
+					userHandle: doc.data().userHandle,
+					createdAt: doc.data().createdAt
+				});
 			});
 			return res.json(posts);
 		})
@@ -29,17 +49,15 @@ exports.getPosts = functions.https.onRequest((req, res) => {
 });
 
 // Find post by name
-exports.findPost = functions.https.onRequest((req, res, name) => {
-	name = req.query.text;
+app.get('/findpost/:postName', (req, res) => {
+	name = req.params.postName;
 	if (name === undefined) {
 		name = 'Pinkman';
 	}
 	if (name === 'undefined') {
 		name = 'Pinkman';
 	}
-	admin
-		.firestore()
-		.collection('Posts')
+	db.collection('Posts')
 		.where('name', '==', name)
 		.get()
 		.then((data) => {
@@ -53,24 +71,18 @@ exports.findPost = functions.https.onRequest((req, res, name) => {
 });
 
 //Create a post function
-exports.createPost = functions.https.onRequest((req, res) => {
-	if (req.method !== 'POST') {
-		return res.status(400).json({ error: 'Method FAILURE' });
-	}
+app.post('/post', (req, res) => {
 	const newPost = {
 		name: req.body.name,
 		bodyAccount: req.body.bodyAccount,
 		bodyResolution: req.body.bodyAccount,
-		createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+		createdAt: new Date().toISOString(),
 		facebookLink: req.body.facebookLink,
 		instagramLink: req.body.instagramLink,
 		otherLink: req.body.otherLink,
 		userHandle: req.body.userHandle
 	};
-
-	admin
-		.firestore()
-		.collection('Posts')
+	db.collection('Posts')
 		.add(newPost)
 		.then((doc) => {
 			return res.json({
@@ -82,3 +94,56 @@ exports.createPost = functions.https.onRequest((req, res) => {
 			console.error(err);
 		});
 });
+
+//signup route
+app.post('/signup', (req, res) => {
+	const newUser = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPassword: req.body.confirmPassword,
+		handle: req.body.handle
+	};
+
+	//Validate data
+
+	let token, userId;
+
+	db.doc(`/users/${newUser.handle}`)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				return res.status(400).json({ handle: 'this handle is already taken' });
+			} else {
+				return firebase
+					.auth()
+					.createUserWithEmailAndPassword(newUser.email, newUser.password);
+			}
+		})
+		.then((data) => {
+			userId = data.user.uid;
+			return data.user.getIdToken();
+		})
+		.then((idToken) => {
+			token = idToken;
+			const userCredentials = {
+				handle: newUser.handle,
+				email: newUser.email,
+				createdAt: new Date().toISOString(),
+				userId: userId
+			};
+			return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+		})
+		.then(() => {
+			return res.status(201).json({ token });
+		})
+		.catch((err) => {
+			console.error(err);
+			if (err.code === 'auth/email-already-in-use') {
+				return res.status(400).json({ email: 'email already in use' });
+			} else {
+				return res.status(500).json({ error: err.code });
+			}
+		});
+});
+
+exports.api = functions.https.onRequest(app);
